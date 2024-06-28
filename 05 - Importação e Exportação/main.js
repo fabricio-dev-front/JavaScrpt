3e4cba4af80511272880c016f5fe725bfc8f8aeb
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-06-28 13:29:13 -0300

    importamos as duas funções criadas

---
+++ 05 - Importação e Exportação/main.js
@@ -0,0 +1,9 @@
+const funcoes = require('./funcoes-auxiliares'); 
+
+// importando as funções da pasta funcoes-auxiliares.js; 
+
+// ou seja, as funcões do arquivo 'funcoes-auxiliares.js' estão aqui tambem agora.
+
+// essas funções vai vai ser recebida aqui como um objeto. 
+
+console.log(funcoes); 
