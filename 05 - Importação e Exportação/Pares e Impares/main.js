8b0cd34d6feaed5c833bfe704a111cd9944d7325
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-07-02 04:20:53 -0300

    melhorando a logica do programa pra ficar mais legível

--- 05 - Importação e Exportação/Pares e Impares/main.js
+++ 05 - Importação e Exportação/Pares e Impares/main.js
@@ -1,20 +1,18 @@
 const {gets, print} = require('./auxiliares');
 
 const quantidadeNumeros = gets();
-let maiorNumeroPar = 0;
-let menorNumeroImpar = 0;
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
 
 for(let i = 0; i < quantidadeNumeros; i++){
     const saidaDeNumros = gets();
     
     if(saidaDeNumros % 2 === 0){
-        if(saidaDeNumros > maiorNumeroPar){
+        if(maiorNumeroPar === null || saidaDeNumros > maiorNumeroPar){
             maiorNumeroPar = saidaDeNumros;
         }
     } else{
-        if(menorNumeroImpar === 0){
-            menorNumeroImpar = saidaDeNumros;
-        } else if(saidaDeNumros < menorNumeroImpar){
+        if(menorNumeroImpar === null || saidaDeNumros < menorNumeroImpar){
             menorNumeroImpar = saidaDeNumros;
         }
     }
